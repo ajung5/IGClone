12204bcb12b40571a823e52cffdde40177c0cbb0
Agung Nawawi <agung@fairtech.com.sg>
2023-12-05 21:04:28 +0700

    Initial Commit

---
+++ IGClone/IGCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  IGCloneApp.swift
+//  IGClone
+//
+//  Created by Agung Nawawi on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct IGCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
