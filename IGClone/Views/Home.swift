99bd9843d22fb21820f8b2b046247f89ee9856d4
Agung Nawawi <agung@fairtech.com.sg>
2023-12-05 21:39:21 +0700

    combine stories and header

--- IGClone/Views/Home.swift
+++ IGClone/Views/Home.swift
@@ -12,16 +12,57 @@ struct Home: View {
     var userStories: UserStoryModel
     
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                ForEach(userStory) { item in
-                    Story(userStories: item)
-                }
-            }
-        }
+        
+        Header()
+        
+        Stories()
+        
+        Divider()
+        
+        Spacer()
+        
+        
     }
 }
 
 #Preview {
     Home(userStories: userStory[5])
+}
+
+
+// MARK: Header
+struct Header: View {
+    var body: some View {
+        HStack {
+            Image("iglogo")
+            
+            Spacer()
+            
+            HStack(spacing: 20) {
+                Image(systemName: "plus.app")
+                    .font(.title)
+                Image(systemName: "heart")
+                    .font(.title)
+                Image("messenger")
+                    .resizable()
+                    .frame(width: 25, height: 25)
+            }
+        }
+        .padding(.horizontal, 15)
+        .padding(.vertical, 3)
+    }
+}
+
+// MARK: Stories
+struct Stories: View {
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(userStory.shuffled()) { item in
+                    Story(userStories: item)
+                }
+            }
+        }
+        .scrollIndicators(.hidden)
+    }
 }
